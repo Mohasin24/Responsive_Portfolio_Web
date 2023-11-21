e7c3da0a8175533978dc93c043996058a26991f2
Mohasin Patel <mohasinpatel313@gmail.com>
2023-11-21 14:55:41 +0530

    v2

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const date = document.getElementById("date");
 const currDate = new Date();
 
 date.textContent= currDate.getFullYear()
+
+function underDev(){
+
+    alert("This functionality is under development! Will be available soon.");
+}
