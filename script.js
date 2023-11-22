b07b33bab757b0da28993f82d1137ac980ddeae3
Mohasin Patel <mohasinpatel313@gmail.com>
2023-11-22 20:06:51 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const date = document.getElementById("date");
-const currDate = new Date();
-
-date.textContent= currDate.getFullYear()
-
+// General Section
 function underDev(){
 
     alert("This functionality is under development! Will be available soon.");
 }
+
+// Download CV Section
+
+let downloadURL = "https://drive.google.com/file/d/1XxYy_gzF3VtmqDkR6dHqSIshK68OHEjY/view?usp=drive_link"
+
+function downloadCV(){
+    
+    let anchor = document.createElement('a');
+
+    anchor.href = downloadURL;
+    anchor.target = "_blank";
+
+    anchor.click();
+}
+// End Download CV Section
+
+
+// Updating the date on footer
+
+const date = document.getElementById("date");
+const currDate = new Date();
+date.textContent= currDate.getFullYear()
+// End
